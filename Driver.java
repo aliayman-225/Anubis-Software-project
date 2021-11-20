213b18efecdb9681043aa21bce74f8493f305835
reemehab <60987184+reemehab@users.noreply.github.com>
2021-11-20 19:14:11 +0200

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -3,16 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package sprint1;
 
 /**
  *
- * @author User
+ * @author ACER
  */
-public class Driver extends User{
-   
-     public Driver(String username,String mobileNumber,String email , String password)
-    {
-        super( username, mobileNumber, email ,  password);
+public class Driver extends User {
+
+    double averageRate = 0.0;
+    int numOfPassengers = 0;
+    private boolean suspendedAccount = true;
+
+    public Driver(String username, String mobileNumber, String email, String password) {
+        super(username, mobileNumber, email, password);
     }
-     
+
+    public boolean getSuspendedAccount() {
+        return suspendedAccount;
+    }
+
+    public void setSuspendedAccount(boolean suspendedAccount) {
+        this.suspendedAccount = suspendedAccount;
+    }
+
+    public double getAverageRate() {
+        return averageRate;
+    }
+
+    public void setAverageRate(double averageRate) {
+        this.averageRate = averageRate;
+    }
+
+    public int getNumOfPassengers() {
+        return numOfPassengers;
+    }
+
+    public void incrementNumOfPassengers() {
+        this.numOfPassengers++;
+    }
+
 }
