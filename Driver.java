5c6f859ce5ccdfe9246ce4a42991b29b0d278b58
aliayman-225 <85026929+aliayman-225@users.noreply.github.com>
2021-11-21 02:18:40 +0200

    Update Driver.java
    this class under construction

--- Driver.java
+++ Driver.java
@@ -4,15 +4,56 @@
  * and open the template in the editor.
  */
 
+
 /**
  *
- * @author User
+ * @author Ali
  */
-public class Driver extends User{
-   
-     public Driver(String username,String mobileNumber,String email , String password)
-    {
-        super( username, mobileNumber, email ,  password);
+public class Driver extends User {
+
+    double averageRate = 0.0;
+    int numOfPassengers = 0;
+     private String driverLicenseNumber,nationalID;
+
+    public String getNationalID() {
+        return nationalID;
     }
-     
+
+    public void setNationalID(String nationalID) {
+        this.nationalID = nationalID;
+    }
+    
+
+    public String getDriverLicenseNumber() {
+        return driverLicenseNumber;
+    }
+
+    public void setDriverLicenseNumber(String driverLicenseNumber) {
+        this.driverLicenseNumber = driverLicenseNumber;
+    }
+   
+    public Driver(String username, String mobileNumber, String email, String password,String driverLicenseNumber ,String nationalID) {
+        super(username, mobileNumber, email, password,UserType.DRIVER);
+        this.driverLicenseNumber=driverLicenseNumber;
+        this.nationalID=nationalID;
+    }
+
+    
+
+    public double getAverageRate() {
+        return averageRate;
+    }
+
+    public void setAverageRate(double averageRate) {
+        this.averageRate = averageRate;
+    }
+
+    public int getNumOfPassengers() {
+        return numOfPassengers;
+    }
+
+    public void incrementNumOfPassengers() {
+        this.numOfPassengers++;
+    }
+
 }
