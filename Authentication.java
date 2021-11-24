b79603ca755e7888c6ac4765ef127481903f92c2
aliayman-225 <85026929+aliayman-225@users.noreply.github.com>
2021-11-24 02:14:54 +0200

    Update Authentication.java

--- Authentication.java
+++ Authentication.java
@@ -2,16 +2,30 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Ali
  */
 public class Authentication {
+
+    private ArrayList<Driver> drivers;
+    private ArrayList<Passenger> passengers;
+    private ArrayList<Ride> rides;
+    private static Authentication instance;
+    Scanner input = new Scanner(System.in);
+
+    private Authentication() {
+        drivers = new ArrayList<Driver>();
+        passengers = new ArrayList<Passenger>();
+        rides = new ArrayList<Ride>();
+    }
+
+    public static Authentication getAuthentication() {
+        if (instance == null) {
+            instance = new Authentication();
+        }
+        return instance;
+    }
 
     public ArrayList<Driver> getDrivers() {
         return drivers;
@@ -29,28 +43,28 @@ public class Authentication {
         this.passengers = passengers;
     }
 
-    private ArrayList<Driver> drivers;
-    private ArrayList<Passenger> passengers;
-    private static Authentication instance;
-    Scanner input = new Scanner(System.in);
-
-    private Authentication() {
-        drivers = new ArrayList<Driver>();
-        passengers = new ArrayList<Passenger>();
+    public ArrayList<Ride> getRides() {
+        return rides;
     }
 
-    public static Authentication getAuthentication() {
-        if (instance == null) {
-            instance = new Authentication();
+    public void setRides(ArrayList<Ride> rides) {
+        this.rides = rides;
+    }
+
+    public boolean checkUser(User user2, User user) // function to check if the username and password matches in the data base
+    {
+        if ((user2.getUsername().equals(user.getUsername())) && (user2.getPassword().equals(user.getPassword()))) {
+            return true;
+        } else {
+            return false;
         }
-        return instance;
     }
 
     public User login(User user) {
         if (user.getClass().getName().equals("Passenger")) {
             for (int i = 0; i < passengers.size(); i++) {
-                if ((passengers.get(i).getUsername().equals(user.getUsername())) && (passengers.get(i).getPassword().equals(user.getPassword()))) {
-                    System.out.println("====== Welcome " + passengers.get(i).getUsername() + " ======");
+                            System.out.println(this.checkUser(passengers.get(i), user));
+                if (this.checkUser(passengers.get(i), user)) {
                     if (passengers.get(i).isSuspendedAccount()) {
                         System.out.println("This account suspended by the admin");
                         return null;
@@ -59,46 +73,46 @@ public class Authentication {
                 }
             }
             System.out.println("Incorrect username or password");
+
         } else if (user.getClass().getName().equals("Driver")) {
             for (int i = 0; i < drivers.size(); i++) {
-                if ((drivers.get(i).getUsername().equals(user.getUsername())) && (drivers.get(i).getPassword().equals(user.getPassword()))) {
-
+                if (this.checkUser(drivers.get(i), user)) {
                     if (drivers.get(i).isSuspendedAccount()) {
                         System.out.println("This account not verified yet by the admin");
                         return null;
                     }
-                    System.out.println("====== Welcome  captain " + drivers.get(i).getUsername() + " ======");
+
                     return drivers.get(i);
                 }
             }
             System.out.println("Incorrect username or password");
+
         }
         return null;
     }
 
     public boolean SignUp(User user) {
-        while (true) {
-            for (int i = 0; i < passengers.size(); i++) {
-                if (user.getUsername().equals(passengers.get(i).getUsername())) {
-                    return false;
-                }
-            }
-            for (int i = 0; i < drivers.size(); i++) {
-                if (user.getUsername().equals(drivers.get(i).getUsername())) {
-                    return false;
-                }
-            }
-            if (user.getClass().getName().equals("Passenger")) {
-                passengers.add((Passenger) user);
-                break;
 
-            } else if (user.getClass().getName().equals("Driver")) {
-                drivers.add((Driver) user);
-                break;
+        for (int i = 0; i < passengers.size(); i++) {
+            if (user.getUsername().equals(passengers.get(i).getUsername())) {
+                return false;
             }
-
         }
-        return true;
-    }
+        for (int i = 0; i < drivers.size(); i++) {
+            if (user.getUsername().equals(drivers.get(i).getUsername())) {
+                return false;
+            }
+        }
+        if (user.getClass().getName().equals("Passenger")) {
+            passengers.add((Passenger) user);
+            return true;
 
+        } else if (user.getClass().getName().equals("Driver")) {
+            drivers.add((Driver) user);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
