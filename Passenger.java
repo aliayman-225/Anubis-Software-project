1bbe1cb3b92a586fea64a5d7427dfe106f67a9e0
aliayman-225 <85026929+aliayman-225@users.noreply.github.com>
2021-11-24 02:15:46 +0200

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,33 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+import java.util.ArrayList;
 
 /**
  *
- * @author ACER
+ * @author Reem
  */
 public class Passenger extends User {
 
+    ArrayList<String> notifications = new ArrayList<String>();
 
     public Passenger(String username, String mobileNumber, String email, String password) {
-        super(username, mobileNumber, email, password,UserType.PASSENGER);
+        super(username, mobileNumber, email, password);
     }
 
-    public boolean requestRide(String source, String destination) //true untile Rider Class is implemented
+    public void rateDriver(int rate, Driver driver) {
+        driver.getRateHistory().add(this.getUsername());
+        driver.getRateHistory().add(String.valueOf(rate));
+        driver.setAverageRate();
+
+    }
+
+    public double checkDriverRate(Driver driver) {
+
+        return driver.getAverageRate();
+    }
+
+    public void requestRide(String source, String destination, Authentication authentication) //true untile Rider Class is implemented
     {
-        return true;
+        Ride ride = new Ride(source, destination, this);
+        for (int i = 0; i < authentication.getDrivers().size(); i++) {
+            if (authentication.getDrivers().get(i).getFavAreas().contains(source)) {
+                authentication.getDrivers().get(i).getPendingRides().add(ride);
+            }
+        }
+        System.out.println("Your ride is waiting for a captain");
+        authentication.getRides().add(ride);
+    }
+
+    public void getNotifications() {
+        for (int i = 0; i < notifications.size(); i++) {
+            System.out.println(notifications.get(i));
+        }
 
     }
 
-    public void rateDriver(int rate) {
-
-    }
-
-    public double checkDriverRate(Driver D) {
-
-        return D.getAverageRate();
-    }
 }
